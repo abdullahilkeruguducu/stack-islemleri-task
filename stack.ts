44c6dafbe76b1c44d79b8e7c10b12daa9aa9fe95
Abdullah Ilker UGUDUCU <abdullahilkeruguducu@gmail.com>
2023-07-10 15:43:29 +0300

    isValidParantheses fonksiyonu yapıldı

--- stack.ts
+++ stack.ts
@@ -21,3 +21,32 @@ class Stack<T> {
     return this.items.length === 0;
   }
 }
+
+function isValidParentheses(input: string): boolean {
+  const stack = new Stack<string>();
+  const openingBrackets = ["(", "{", "["];
+  const closingBrackets = [")", "}", "]"];
+
+  for (let i = 0; i < input.length; i++) {
+    const char = input[i];
+
+    if (openingBrackets.includes(char)) {
+      stack.push(char);
+    } else if (closingBrackets.includes(char)) {
+      const lastOpeningBracket = stack.pop();
+
+      if (
+        lastOpeningBracket === undefined ||
+        openingBrackets.indexOf(lastOpeningBracket) !==
+          closingBrackets.indexOf(char)
+      ) {
+        return false;
+      }
+    }
+  }
+
+  return stack.isEmpty();
+}
+
+console.log(isValidParentheses("asdkj{kasjdk[jashd{akjsdh}asd]asd}asd")); // true
+console.log(isValidParentheses("{[()]")); // false
